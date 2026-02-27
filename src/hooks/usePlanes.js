2c821a5ffe757aba7cd88a6f7822f9ecd60e1911
Yeimar Araujo <yeimararaujomedina@gmail.com>
2026-02-26 22:06:54 -0500

    arreglar

--- src/hooks/usePlanes.js
+++ src/hooks/usePlanes.js
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { API_URL } from "../api/config";
+import axiosClient from "../api/axiosClient";
+
 export const usePlanes = () => {
   return useQuery({
     queryKey: ["planes"],
     queryFn: async () => {
-      const response = await fetch(`${API_URL}/planes`);
+      const response = await axiosClient.get(`/planes`);
 
        if (!response.ok) {
         throw new Error(`HTTP ${response.status}`);
